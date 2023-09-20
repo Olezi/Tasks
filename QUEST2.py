565b4bb489935c36045cb9edaac75bb82cbecf23
Olezi <67165570+Olezi@users.noreply.github.com>
2023-09-20 19:45:19 -0300

    Update QUEST2.py

--- QUEST2.py
+++ QUEST2.py
@@ -1,15 +1,15 @@
 def PVZ(zombies, plants):
-    # Обчислюємо суми значень в масивах
+  
     zombies_strength = sum(zombies)
     plants_strength = sum(plants)
     
-    # Порівнюємо суми і враховуємо правила
+   
     if zombies_strength > plants_strength:
         return False
     elif zombies_strength < plants_strength:
         return True
     else:
-        # Якщо суми рівні, порівнюємо початкову кількість зомбі і рослин
+       
         if len(zombies) > len(plants):
             return False
         elif len(zombies) < len(plants):
@@ -17,7 +17,7 @@ def PVZ(zombies, plants):
         else:
             return True
 
-# Проводимо тести
+
 zombies1 = [1, 3, 5, 7]
 plants1 = [2, 4, 6, 8]
 print(PVZ(zombies1, plants1)) 
